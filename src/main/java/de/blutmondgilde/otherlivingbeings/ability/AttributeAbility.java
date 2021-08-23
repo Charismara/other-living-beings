194fac311a590eb8d4481132d9d22eee5d039853
Charismara <craoulis@gmail.com>
2021-08-23 10:55:08 +0200

    fix player dies on attribute apply

--- src/main/java/de/blutmondgilde/otherlivingbeings/ability/AttributeAbility.java
+++ src/main/java/de/blutmondgilde/otherlivingbeings/ability/AttributeAbility.java
@@ -2,7 +2,6 @@ package de.blutmondgilde.otherlivingbeings.ability;
 
 import de.blutmondgilde.otherlivingbeings.api.abilities.Ability;
 import de.blutmondgilde.otherlivingbeings.api.abilities.listener.AttributeUpdateListener;
-import lombok.Getter;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
@@ -12,14 +11,11 @@ import net.minecraft.world.entity.player.Player;
 import java.util.UUID;
 
 public class AttributeAbility extends Ability implements AttributeUpdateListener {
-    @Getter
-    private final float value;
     private final AttributeModifier attributeModifier;
     private final Attribute attribute;
 
     public AttributeAbility(final TranslatableComponent name, final TranslatableComponent[] description, final Attribute attribute, final UUID uuid, final String modifierName, final float value) {
         super(name, description);
-        this.value = value;
         this.attributeModifier = new AttributeModifier(uuid, modifierName, value, AttributeModifier.Operation.ADDITION);
         this.attribute = attribute;
     }
@@ -32,7 +28,7 @@ public class AttributeAbility extends Ability implements AttributeUpdateListener
         }
         attributeInstance.addPermanentModifier(attributeModifier);
         //Trigger Attribute update
-        player.setHealth(Math.min(player.getHealth(), (float) attributeInstance.getValue()));
+        player.setHealth(Math.min(player.getHealth(), player.getMaxHealth()));
     }
 
     @Override
