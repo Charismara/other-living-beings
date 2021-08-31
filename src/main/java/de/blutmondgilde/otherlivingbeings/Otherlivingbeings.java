c63aeccbd528846dc66fa3b968d9c9656e0fb7ec
Charismara <craoulis@gmail.com>
2021-09-01 00:26:34 +0200

    initial commit

---
+++ src/main/java/de/blutmondgilde/otherlivingbeings/Otherlivingbeings.java
@@ -0,0 +1,10 @@
+package de.blutmondgilde.otherlivingbeings;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Otherlivingbeings implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/de/blutmondgilde/otherlivingbeings/client/OtherlivingbeingsClient.java
@@ -0,0 +1,13 @@
+package de.blutmondgilde.otherlivingbeings.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class OtherlivingbeingsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
